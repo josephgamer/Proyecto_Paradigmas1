1ea86e3ac20c33e04a3f25bde0d7cf6573b2caf0
Esteban <jose.gonzalez.h@est.una.ac.cr>
2020-10-06 17:51:30 -0600

    Se corre la ventana principal con exito

--- Project1/src/project1/Project1.java
+++ Project1/src/project1/Project1.java
@@ -20,11 +20,17 @@ public class Project1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        Servicio servicio = new Servicio();
+        /*Servicio servicio = new Servicio();
         List<Expresiones> list = servicio.cargarDesdeArchivo();
         for (Expresiones expresion : list) {
             System.out.println(expresion.getExpresion());
-        }
+        }*/
+        controller.ModelTable model = new controller.ModelTable();
+        view.View view = new view.View();
+        controller.Controller controller = new controller.Controller(model, view);
+        view.setVisible(true);
+        LISTADO_EXPRESIONES = controller;
     }
     
+    public static controller.Controller LISTADO_EXPRESIONES;
 }
