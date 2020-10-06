5e361389d924cbaf419461f8abd258ece0736044
Esteban <jose.gonzalez.h@est.una.ac.cr>
2020-10-06 17:36:31 -0600

    Se crea el metodo que carga los datos desde un archivo

--- Project1/src/controller/Controller.java
+++ Project1/src/controller/Controller.java
@@ -5,6 +5,9 @@
  */
 package controller;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import logica.Model;
 import view.View;
 
@@ -13,13 +16,23 @@ import view.View;
  * @author Esteban
  */
 public class Controller {
-    Model model;
+    ModelTable model;
     View view;
     
-    public Controller(Model model, View view) {
+    public Controller(ModelTable model, View view) {
         this.model = model;
         this.view = view;
-        view.setModel(model);
+        view.setModelTable(model);
         view.setController(this);
+    }
+    
+    public void cargarDatos() {
+        try {
+            model.setExpresiones(service.Servicio.instance().cargarDesdeArchivo());
+            model.setTableExpresiones(service.Servicio.instance().cargarDesdeArchivo());
+        } catch (IOException ex) {
+            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 }
