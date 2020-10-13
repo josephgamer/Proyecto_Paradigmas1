2d53c5af5506ce797383c180506f6baf0994b68e
Esteban <jose.gonzalez.h@est.una.ac.cr>
2020-10-13 17:52:25 -0600

    Cambios en la vista

--- Project1/src/view/SecundaryView.java
+++ Project1/src/view/SecundaryView.java
@@ -12,6 +12,7 @@ import logica.DibujarTabla;
  * @author Esteban
  */
 public class SecundaryView extends javax.swing.JFrame {
+
     DibujarTabla d = new DibujarTabla();
 
     /**
@@ -20,6 +21,7 @@ public class SecundaryView extends javax.swing.JFrame {
     public SecundaryView() {
         initComponents();
         this.jtPositivo.setEditable(false);
+        this.jtnegativo.setEditable(false);
         this.setLocationRelativeTo(null);
     }
 
@@ -42,6 +44,10 @@ public class SecundaryView extends javax.swing.JFrame {
         jtPositivo = new javax.swing.JTextArea();
         jlDescription1 = new javax.swing.JLabel();
         jlvariablespositivas = new javax.swing.JLabel();
+        jlDescription2 = new javax.swing.JLabel();
+        jlvariablesnegativas = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jtnegativo = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -100,6 +106,14 @@ public class SecundaryView extends javax.swing.JFrame {
 
         jlvariablespositivas.setText("hola");
 
+        jlDescription2.setText("Variables Positivas");
+
+        jlvariablesnegativas.setText("hola2");
+
+        jtnegativo.setColumns(20);
+        jtnegativo.setRows(5);
+        jScrollPane2.setViewportView(jtnegativo);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -115,6 +129,12 @@ public class SecundaryView extends javax.swing.JFrame {
                         .addGroup(layout.createSequentialGroup()
                             .addGap(19, 19, 19)
                             .addComponent(jlDescription1, javax.swing.GroupLayout.PREFERRED_SIZE, 119, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addGap(44, 44, 44)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(jlDescription2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jlvariablesnegativas, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -122,11 +142,17 @@ public class SecundaryView extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jlDescription1)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jlDescription1)
+                    .addComponent(jlDescription2))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jlvariablespositivas, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jlvariablespositivas, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jlvariablesnegativas))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 307, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 307, Short.MAX_VALUE)
+                    .addComponent(jScrollPane2))
                 .addContainerGap())
         );
 
@@ -134,7 +160,10 @@ public class SecundaryView extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnEvaluarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEvaluarActionPerformed
-        this.jtPositivo.append(d.devuelveVariablesPositivas());
+        if (this.jtPositivo.getText().isEmpty() && this.jtnegativo.getText().isEmpty()) {
+            this.jtPositivo.append(d.devuelveVariablesPositivas());
+            this.jtnegativo.append(d.devuelveVariablesNegativas());
+        }
     }//GEN-LAST:event_btnEvaluarActionPerformed
 
     /**
@@ -178,10 +207,14 @@ public class SecundaryView extends javax.swing.JFrame {
     private javax.swing.JButton btnModificar;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTextField jfexpresion;
     private javax.swing.JLabel jlDescription1;
+    private javax.swing.JLabel jlDescription2;
     private javax.swing.JLabel jlexpresion;
+    private javax.swing.JLabel jlvariablesnegativas;
     private javax.swing.JLabel jlvariablespositivas;
     private javax.swing.JTextArea jtPositivo;
+    private javax.swing.JTextArea jtnegativo;
     // End of variables declaration//GEN-END:variables
 }
