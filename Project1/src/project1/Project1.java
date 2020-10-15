ca74270159c2b0f396d14fbfaa218f24fae01bdf
Esteban <jose.gonzalez.h@est.una.ac.cr>
2020-10-15 16:54:37 -0600

    Se agregan cosas

--- Project1/src/project1/Project1.java
+++ Project1/src/project1/Project1.java
@@ -7,6 +7,7 @@ package project1;
 
 import java.io.IOException;
 import java.util.List;
+import logica.DibujarTabla;
 import logica.Expresiones;
 import service.Servicio;
 
@@ -30,7 +31,15 @@ public class Project1 {
         controller.Controller controller = new controller.Controller(model, view);
         view.setVisible(true);
         LISTADO_EXPRESIONES = controller;
+        
+        logica.Model secundary = new logica.Model();
+        view.SecundaryView sv = new view.SecundaryView();
+        controller.SecundaryController cs = new controller.SecundaryController(secundary, sv);
+        //sv.setVisible(true);
+        SECUNDARY_CONTROLLER = cs;
+        
     }
-    
+
     public static controller.Controller LISTADO_EXPRESIONES;
+    public static controller.SecundaryController SECUNDARY_CONTROLLER;
 }
